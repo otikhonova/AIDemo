cde7f6597a334d9d682e679ed3dbf97686ea8802
Olga Tikhonova <o.tikhonova@godeltech.com>
2025-06-10 11:29:07 +0200

    Add lecturer CRUD functionality and update docs

---
+++ src/app/components/lecturer/add/lecturer-add.component.spec.ts
@@ -0,0 +1,33 @@
+/**
+ * Created By : Sangwin Gawande (https://sangw.in)
+ */
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LecturerAddComponent } from './lecturer-add.component';
+
+describe('LecturerAddComponent', () => {
+  let component: LecturerAddComponent;
+  let fixture: ComponentFixture<LecturerAddComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LecturerAddComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LecturerAddComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+/**
+ * Created By : Sangwin Gawande (https://sangw.in)
+ */
---
+++ src/app/components/lecturer/details/lecturer-details.component.spec.ts
@@ -0,0 +1,31 @@
+/**
+ * Created By : Sangwin Gawande (https://sangw.in)
+ */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LecturerDetailsComponent } from './lecturer-details.component';
+
+describe('LecturerDetailsComponent', () => {
+  let component: LecturerDetailsComponent;
+  let fixture: ComponentFixture<LecturerDetailsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LecturerDetailsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LecturerDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+/**
+ * Created By : Sangwin Gawande (https://sangw.in)
+ */
